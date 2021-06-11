ffcb2f4981b138c3c7fb3b8993bfeab918c099a9
BenedictusAryo <benedictusaryo@outlook.com>
2021-06-11 23:05:51 +0700

    exercise calculator add

---
+++ Exercise_1_Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator App");
+            Console.WriteLine("");
+            Console.WriteLine("Add 2 numbers");
+            Console.Write("First number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{firstNumber} + {secondNumber} = " + firstNumber + secondNumber);
+
+        }
+    }
+}
